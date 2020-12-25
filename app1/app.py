afbc23801190becc4a6d427fbd805ccabc8aa0bf
Ubuntu <keyur@tresata.com>
2020-12-25 19:33:09 +0000

    flask web app with redis

---
+++ app1/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from redis import Redis
+import os
+
+app = Flask(__name__)
+redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'), port=6379)
+
+@app.route('/')
+def hello():
+    redis.incr('hits')
+    return '#####_APP1_#####\nHello This is APP1! I have been seen %s times.\n' % redis.get('hits').decode('utf-8')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
